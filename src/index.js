8e2770a99c64555409ea197a614beaa10b42dd79
ethyl2 <zzzuzz@gmail.com>
2022-03-17 21:54:55 -0600

    Refactor: moves js into its own file

---
+++ src/index.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    localStorage.theme = 'dark'
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  })
+
+const toggleButton = document.getElementById('mode-toggle')
+toggleButton.addEventListener('click', () => {
+  if (localStorage.theme === 'dark') {
+    document.documentElement.classList.remove('dark')
+    localStorage.theme = 'light'
+  } else {
+    document.documentElement.classList.add('dark')
+    localStorage.theme = 'dark'
+  }
+})
